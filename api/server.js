9c5764b0d74c64eed19adb79f2bbe741a74c6ae2
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-02-07 19:47:55 -0500

    finished project.

--- api/server.js
+++ api/server.js
@@ -15,7 +15,7 @@ server.post('/api/users', (req, res) => {
 	} else {
 		insert(body)
 			.then((user) => {
-				res.status(201).json({ msg: user });
+				res.status(201).json(user);
 			})
 			.catch((err) => {
 				res.status(500).json({
@@ -28,7 +28,7 @@ server.post('/api/users', (req, res) => {
 server.get('/api/users', (req, res) => {
 	find()
 		.then((users) => {
-			res.status(200).json({ msg: users });
+			res.status(200).json(users);
 		})
 		.catch(() => {
 			res
@@ -41,29 +41,63 @@ server.get('/api/users/:id/', (req, res) => {
 	const id = req.params.id;
 	findById(id)
 		.then((user) => {
-			if (!user) {
-				res.status(404).res.json({
-					message: 'The user with the specified ID does not exist',
-				});
-			} else {
+			if (user.id === id) {
 				res.status(200).json(user);
+			} else {
+				res.status(404).json({ message: 'does not exist' });
 			}
 		})
 		.catch(() => {
-			res
-				.status(500)
-				.json({ message: 'The user information could not be retrieved' });
+			res.status(404).json({ message: 'does not exist' });
 		});
 });
 
 server.delete('/api/users/:id', (req, res) => {
 	const id = req.params.id;
-	res.status(200).json({ message: `Deleting user with an ID of ${id} 👀` });
+	findById(id).then((user) => {
+		if (user) {
+			remove(user.id)
+				.then((deletedUser) => {
+					res.status(200).json(deletedUser);
+				})
+				.catch(() => {
+					res.status(500).json({ message: 'The user could not be removed' });
+				});
+		} else {
+			res
+				.status(404)
+				.json({ message: 'The user with the specified ID does not exist' });
+		}
+	});
 });
 
 server.put('/api/users/:id', (req, res) => {
 	const { id } = req.params;
-	res.status(200).json({ message: `Updating user with an ID of ${id} 👌` });
+	const body = req.body;
+
+	if (!body.name || !body.bio) {
+		res
+			.status(400)
+			.json({ message: 'Please provide name and bio for the user' });
+	} else {
+		findById(id).then((user) => {
+			if (user) {
+				update(user.id, body)
+					.then((updated) => {
+						res.status(200).json(updated);
+					})
+					.catch(() => {
+						res
+							.status(500)
+							.json({ message: 'The user information could not be modified' });
+					});
+			} else {
+				res
+					.status(404)
+					.json({ message: 'The user with the specified ID does not exist' });
+			}
+		});
+	}
 });
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
